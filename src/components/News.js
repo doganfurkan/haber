7105c137882c789175a09578a00866f0e256e0bd
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 01:49:41 +0300

    couple changes

--- src/components/News.js
+++ src/components/News.js
@@ -1,30 +1,78 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./news.css";
-import { PiHeart, PiChatCircle, PiShareNetwork } from "react-icons/pi";
+import {
+  PiHeart,
+  PiHeartFill,
+  PiChatCircle,
+  PiShareNetwork,
+  PiCopy,
+  PiWhatsappLogo,
+} from "react-icons/pi";
+var interactionNumber = Math.round(Math.random() * 100);
+var commentNumber = Math.round(
+  interactionNumber * ((10 + Math.random() * 20) / 100)
+);
 
-export default function News({item}) {
-  const [shareButton,setShareButton] = useState("passive");
-  var interactionNumber = Math.round(Math.random()*100);
+export default function News({ item }) {
+  const [shareButton, setShareButton] = useState("passive");
+  const [likeButton, setLikeButton] = useState(false);
 
   return (
     <div className="news">
-      <div className="imgAndButtons" style={{backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(${item.image})`}}>
+      <div
+        className="imgAndButtons"
+        style={{
+          backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(${item.image})`,
+        }}
+      >
         <div className="newsButtonsContainer">
-          <div className="newsWriter">{/*<img src="https://picsum.photos/200" alt="writer" /> */}<p>{item.source}</p></div>
+          <div className="newsWriter">
+            {/*<img src="https://picsum.photos/200" alt="writer" /> */}
+            <p>{item.source}</p>
+          </div>
           <div className="newsActions">
-            <button className="like"><PiHeart/> {interactionNumber}</button>
-            <button className="comment"><PiChatCircle/> {Math.round(interactionNumber * ((10 + Math.random()*20) / 100))}</button>
-            <div className={`share ${shareButton}`} onClick={() => {
-              shareButton === "passive" ? setShareButton("active") : setShareButton("passive")
-            }}><PiShareNetwork/> <div className="shareMenu" onClick={() => {
-              navigator.clipboard.writeText(item.url)
-              .then(() => {
-                alert("successfully copied");
-              })
-              .catch(() => {
-                alert("something went wrong");
-              });     
-            }}>Haber Linkini Kopyala</div></div>
+            <button
+              className={likeButton ? "like liked" : "like"}
+              onClick={() => {
+                setLikeButton(!likeButton);
+              }}
+            >
+              {likeButton ? <PiHeartFill /> : <PiHeart />}{" "}
+              {!likeButton ? interactionNumber : interactionNumber + 1}
+            </button>
+            <button className="comment">
+              <PiChatCircle /> {commentNumber}
+            </button>
+            <div
+              className={`share ${shareButton}`}
+              onClick={() => {
+                shareButton === "passive"
+                  ? setShareButton("active")
+                  : setShareButton("passive");
+              }}
+            >
+              <PiShareNetwork />{" "}
+              <div className="shareMenu">
+                <div
+                  className="copyLink"
+                  onClick={() => {
+                    navigator.clipboard
+                      .writeText(item.url)
+                      .then(() => {
+                        alert("successfully copied");
+                      })
+                      .catch(() => {
+                        alert("something went wrong");
+                      });
+                  }}
+                >
+                  <PiCopy /> Haber Linkini Kopyala
+                </div>
+                <div className="shareWhatsapp">
+                  <PiWhatsappLogo /> Whatsapp ile Paylaş
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -34,8 +82,17 @@ export default function News({item}) {
           <p>{item.description}</p>
         </div>
         <div className="dateAndMore">
-          <div className="newsDate">{item.publishedAt ? item.publishedAt : item.source}</div>
-          <a href={item.url} target="_blank" rel="noreferrer" className="readMore">Devamını Oku {">"}</a>
+          <div className="newsDate">
+            {item.publishedAt ? item.publishedAt : item.source}
+          </div>
+          <a
+            href={item.url}
+            target="_blank"
+            rel="noreferrer"
+            className="readMore"
+          >
+            Devamını Oku {">"}
+          </a>
         </div>
       </div>
     </div>
