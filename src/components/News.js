86f125b916e8ac36710b43c7b27a703311e0aaa8
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 01:14:10 +0300

    random fav and comment numbers

--- src/components/News.js
+++ src/components/News.js
@@ -1,17 +1,30 @@
-import React from "react";
+import {useState} from "react";
 import "./news.css";
 import { PiHeart, PiChatCircle, PiShareNetwork } from "react-icons/pi";
 
 export default function News({item}) {
+  const [shareButton,setShareButton] = useState("passive");
+  var interactionNumber = Math.round(Math.random()*100);
+
   return (
     <div className="news">
       <div className="imgAndButtons" style={{backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(${item.image})`}}>
         <div className="newsButtonsContainer">
           <div className="newsWriter">{/*<img src="https://picsum.photos/200" alt="writer" /> */}<p>{item.source}</p></div>
           <div className="newsActions">
-            <button className="like"><PiHeart/> 50</button>
-            <button className="comment"><PiChatCircle/> 6</button>
-            <button className="share"><PiShareNetwork/></button>
+            <button className="like"><PiHeart/> {interactionNumber}</button>
+            <button className="comment"><PiChatCircle/> {Math.round(interactionNumber * ((10 + Math.random()*20) / 100))}</button>
+            <div className={`share ${shareButton}`} onClick={() => {
+              shareButton === "passive" ? setShareButton("active") : setShareButton("passive")
+            }}><PiShareNetwork/> <div className="shareMenu" onClick={() => {
+              navigator.clipboard.writeText(item.url)
+              .then(() => {
+                alert("successfully copied");
+              })
+              .catch(() => {
+                alert("something went wrong");
+              });     
+            }}>Haber Linkini Kopyala</div></div>
           </div>
         </div>
       </div>
