d517d6d59856f68a3b56e3100c950c49b84b8f65
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 00:00:11 +0300

    writer name is repositioned due to api restrictions

--- src/components/News.js
+++ src/components/News.js
@@ -7,7 +7,7 @@ export default function News({item}) {
     <div className="news">
       <div className="imgAndButtons" style={{backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url(${item.image})`}}>
         <div className="newsButtonsContainer">
-          <div className="newsWriter"><img src="https://picsum.photos/200" alt="writer" /><p>{item.source}</p></div>
+          <div className="newsWriter">{/*<img src="https://picsum.photos/200" alt="writer" /> */}<p>{item.source}</p></div>
           <div className="newsActions">
             <button className="like"><PiHeart/> 50</button>
             <button className="comment"><PiChatCircle/> 6</button>
@@ -21,7 +21,7 @@ export default function News({item}) {
           <p>{item.description}</p>
         </div>
         <div className="dateAndMore">
-          <div className="newsDate">{item.publishedAt}</div>
+          <div className="newsDate">{item.publishedAt ? item.publishedAt : item.source}</div>
           <a href={item.url} target="_blank" rel="noreferrer" className="readMore">Devamını Oku {">"}</a>
         </div>
       </div>
