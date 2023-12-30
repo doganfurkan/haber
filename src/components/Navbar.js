ff8c51a94741ae8d0374d0c8669060b184edadcc
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 16:53:02 +0300

    active page indicator

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,38 @@ import React from "react";
 import "./navbar.css";
 import {
   PiHouseFill,
+  PiHouse,
   PiMagnifyingGlass,
+  PiMagnifyingGlassFill,
   PiNewspaper,
   PiUser,
+  PiNewspaperFill,
+  PiUserFill,
 } from "react-icons/pi";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav id="nav">
-      <NavLink
-        to={"/"}
-        className="menuButton"
-      >
-        <PiHouseFill />
+      <NavLink to={"/"} className="menuButton">
+        {({ isActive }) => (
+          isActive ? <PiHouseFill/> : <PiHouse/>
+        )}
       </NavLink>
       <NavLink to={"search"} className="menuButton">
-        <PiMagnifyingGlass />
+      {({ isActive }) => (
+          isActive ? <PiMagnifyingGlassFill/> : <PiMagnifyingGlass/>
+        )}
       </NavLink>
       <NavLink to={"news"} className="menuButton">
-        <PiNewspaper />
+      {({ isActive }) => (
+          isActive ? <PiNewspaperFill/> : <PiNewspaper/>
+        )}
       </NavLink>
       <NavLink to={"profile"} className="menuButton">
-        <PiUser />
+      {({ isActive }) => (
+          isActive ? <PiUserFill/> : <PiUser/>
+        )}
       </NavLink>
     </nav>
   );
