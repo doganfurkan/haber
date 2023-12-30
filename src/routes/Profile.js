9dd6cca97862a1dd187463adb90be974077338aa
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 20:34:05 +0300

    image shape changed

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import "./profile.css"
+import React from "react";
+import "./profile.css";
+import { PiGear, PiEnvelopeSimple, PiInfo, PiQuestion } from "react-icons/pi";
+
 
 export default function Profile() {
   return (
-    <div id='profile'>Profile</div>
-  )
+    <div id="profile">
+      <h1>Profil</h1>
+      <div id="myProfile">
+        <img
+          src="https://picsum.photos/200"
+          alt="profile name"
+          id="profileImg"
+        />
+        <div id="profileDetail">
+          <div id="profileName">Misafir Kullanıcı</div>
+          <div id="profilesubtext">Giriş yapın veya kaydolun</div>
+        </div>
+      </div>
+      <div id="profileList">
+        <button><PiGear/>Ayarlar ve Gizlilik</button>
+        <button><PiEnvelopeSimple/>Bize Ulaşın</button>
+        <button><PiQuestion/>Sıkça Sorulan Sorular</button>
+        <button><PiInfo/>Hakkımızda</button>
+      </div>
+    </div>
+  );
 }
