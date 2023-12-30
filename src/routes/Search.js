26406ca550064af7e3d0ed87f920ebcae3eb5fe1
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 19:48:02 +0300

    categories backgrounds

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -13,18 +13,18 @@ export default function Search() {
         </button>
       </form>
       <div id="categories">
-        <div className="category">Gündem</div>
-        <div className="category">Spor</div>
-        <div className="category">Siyaset</div>
-        <div className="category">Dünya</div>
-        <div className="category">Magazin</div>
-        <div className="category">Ekonomi</div>
-        <div className="category">Teknoloji</div>
-        <div className="category">Hayat</div>
-        <div className="category">Bilim</div>
-        <div className="category">Köşe Yazıları</div>
-        <div className="category">Yurtdışı</div>
-        <div className="category">Oyun</div>
+        <div className="category" data-content="Gündem">Gündem</div>
+        <div className="category" data-content="Spor">Spor</div>
+        <div className="category" data-content="Siyaset">Siyaset</div>
+        <div className="category" data-content="Dünya">Dünya</div>
+        <div className="category" data-content="Magazin">Magazin</div>
+        <div className="category" data-content="Ekonomi">Ekonomi</div>
+        <div className="category" data-content="Teknoloji">Teknoloji</div>
+        <div className="category" data-content="Hayat">Hayat</div>
+        <div className="category" data-content="Bilim">Bilim</div>
+        <div className="category" data-content="Köşe Yazıları">Köşe Yazıları</div>
+        <div className="category" data-content="Yurtdışı">Yurtdışı</div>
+        <div className="category" data-content="Oyun">Oyun</div>
       </div>
     </div>
   );
