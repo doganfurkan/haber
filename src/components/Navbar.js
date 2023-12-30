e884fc144448b5076313125cf41367bd46562085
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 22:53:54 +0300

    desktop responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,26 +15,71 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav id="nav">
-      <NavLink to={"/"} className="menuButton">
-        {({ isActive }) => (
-          isActive ? <PiHouseFill/> : <PiHouse/>
-        )}
-      </NavLink>
-      <NavLink to={"search"} className="menuButton">
-      {({ isActive }) => (
-          isActive ? <PiMagnifyingGlassFill/> : <PiMagnifyingGlass/>
-        )}
-      </NavLink>
-      <NavLink to={"news"} className="menuButton">
-      {({ isActive }) => (
-          isActive ? <PiNewspaperFill/> : <PiNewspaper/>
-        )}
-      </NavLink>
-      <NavLink to={"profile"} className="menuButton">
-      {({ isActive }) => (
-          isActive ? <PiUserFill/> : <PiUser/>
-        )}
-      </NavLink>
+      <div id="navLogo">
+        haber<span>ci</span>
+      </div>
+      <div id="navContainer">
+        <NavLink to={"/"} className="menuButton">
+          {({ isActive }) =>
+            isActive ? (
+              <>
+                <PiHouseFill />
+                <p>Anasayfa</p>
+              </>
+            ) : (
+              <>
+                <PiHouse />
+                <p>Anasayfa</p>
+              </>
+            )
+          }
+        </NavLink>
+        <NavLink to={"search"} className="menuButton">
+          {({ isActive }) =>
+            isActive ? (
+              <>
+                <PiMagnifyingGlassFill />
+                <p>Arama</p>
+              </>
+            ) : (
+              <>
+                <PiMagnifyingGlass />
+                <p>Arama</p>
+              </>
+            )
+          }
+        </NavLink>
+        <NavLink to={"news"} className="menuButton">
+          {({ isActive }) =>
+            isActive ? (
+              <>
+                <PiNewspaperFill />
+                <p>Tüm Haberler</p>
+              </>
+            ) : (
+              <>
+                <PiNewspaper />
+                <p>Tüm Haberler</p>
+              </>
+            )
+          }
+        </NavLink>
+        <NavLink to={"profile"} className="menuButton">
+          {({ isActive }) =>
+            isActive ? (
+              <>
+                <PiUserFill />
+                <p>Profil</p>
+              </>
+            ) : (
+              <>
+                <PiUser />
+                <p>Profil</p>
+              </>
+            )
+          }
+        </NavLink>
+      </div>
     </nav>
   );
 }
