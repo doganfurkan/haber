8054d79a2e2ffd554c4d1deb6a7f7db72f21afda
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 20:33:51 +0300

    filter added

--- src/routes/Allnews.js
+++ src/routes/Allnews.js
@@ -2,13 +2,26 @@ import React from "react";
 import "./allnews.css";
 import Smallnews from "../components/Smallnews";
 import { useSelector } from "react-redux";
+import { PiFunnel } from "react-icons/pi";
 
 export default function Allnews() {
   const myNews = useSelector((state) => state.news.news);
 
   return (
     <div id="allnews">
-        <h1>Tüm Haberler</h1>
+      <h1>Tüm Haberler</h1>
+      <div id="allNewsFilter">
+        <div id="popularCategories">
+          <button>Gündem</button>
+          <button>Spor</button>
+          <button>Siyaset</button>
+          <button>Teknoloji</button>
+          <button>Ekonomi</button>
+        </div>
+        <button id="filterButton">
+          <PiFunnel/> Filtrele
+        </button>
+      </div>
       <div id="allnewscontainer">
         <div id="allnewsleft">
           {myNews.result.map((item, key) => {
