fae3b7bf6c25b53b851a2bcb56da2f7d5217c9fb
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 20:54:55 +0300

    alt attribute for image

--- src/components/Smallnews.js
+++ src/components/Smallnews.js
@@ -4,7 +4,7 @@ import "./smallnews.css"
 export default function smallNews({item}) {
   return (
     <div className='smallNews'>
-        <div className="smallNewsImage"><img src={item.image} alt="image"/></div>
+        <div className="smallNewsImage"><img src={item.image} alt="haber-resmi"/></div>
         <div className="smallNewsContent">
             <h1>{item.name}</h1>
         </div>
