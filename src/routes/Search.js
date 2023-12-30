99f85f79b96e83d3e8ab1b9a1911659a16932b62
Furkan Doğan <1furkandogan1@gmail.com>
2023-12-30 17:49:47 +0300

    Search page

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,8 +1,31 @@
-import React from 'react'
-import "./search.css"
+import React from "react";
+import "./search.css";
+import { PiMagnifyingGlass } from "react-icons/pi";
 
 export default function Search() {
   return (
-    <div id='search'>Search</div>
-  )
+    <div id="search">
+      <h1>Arama</h1>
+      <form action="#" id="searchBox">
+        <input type="search" name="searchInput" id="searchInput" placeholder="Tüm Haberler arasında arama yap"/>
+        <button id="searchButton">
+          <PiMagnifyingGlass />
+        </button>
+      </form>
+      <div id="categories">
+        <div className="category">Gündem</div>
+        <div className="category">Spor</div>
+        <div className="category">Siyaset</div>
+        <div className="category">Dünya</div>
+        <div className="category">Magazin</div>
+        <div className="category">Ekonomi</div>
+        <div className="category">Teknoloji</div>
+        <div className="category">Hayat</div>
+        <div className="category">Bilim</div>
+        <div className="category">Köşe Yazıları</div>
+        <div className="category">Yurtdışı</div>
+        <div className="category">Oyun</div>
+      </div>
+    </div>
+  );
 }
